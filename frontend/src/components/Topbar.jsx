ef0f862e508a75bac4b36eb46eae32b88659253b
Inna-decsgr <anjgkwl27@gmail.com>
2025-03-22 16:54:03 +0900

    feat: Add conditional UI and layout feature to Topbar based on login state

--- frontend/src/components/Topbar.jsx
+++ frontend/src/components/Topbar.jsx
@@ -1,17 +1,39 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import { Link } from "react-router-dom";
+import { useAuth } from '../context/AuthContext';
 
 
 export default function Topbar() {
+  const { state, dispatch } = useAuth();
+
+  useEffect(() => {
+    console.log('로그인한 사용자', JSON.stringify(state.user));
+  })
+
+  const handleLogout = () => {
+    // 로그아웃 로직
+    dispatch({ type: 'LOGOUT' });  // 사용자 전역 상태 초기화
+    localStorage.removeItem('user');  // 로컬스토리지에서 사용자 정보 삭제
+  }
+  
+
   return (
     <header className='w-full bg-red-100 p-4 flex justify-center'>
-      <div className='max-w-screen-xl w-full flex justify-between'>
-        <p>(비공개 계정일 경우 자물쇠 아이콘) 사용자 아이디</p>
-        <div className='flex space-x-4'>
-          <button>새 게시글 작성 아이콘</button>
-          <button>설정 아이콘</button>
+      {state.user ? (
+        <div className='max-w-screen-xl w-full flex justify-between'>
+          <p>(<i class="fa-solid fa-lock"></i>){state.user.userid}</p>
+          <div className='flex space-x-4'>
+            <button><i class="fa-solid fa-square-plus"></i></button>
+            <button><i class="fa-solid fa-gear"></i></button>
+            <button onClick={handleLogout}>로그아웃</button>
+          </div>
         </div>
-    </div>
+      ) : (
+        <div>
+          <p>linkup</p>
+          <Link to='/login'><button>로그인</button></Link>
+        </div>
+      )}
     </header>
   );
 }
