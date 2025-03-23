8c48848a43d804df242e9d98bd2db70cd58bc511
Inna-decsgr <anjgkwl27@gmail.com>
2025-03-23 19:03:00 +0900

    Create user settings page and connect user profile edit component

---
+++ frontend/src/pages/UserSetting.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { useAuth } from '../context/AuthContext';
+import EditUserProfile from '../components/EditUserProfile';
+
+
+export default function UserSetting() {
+  const { state } = useAuth();
+  const [isEdit, setisEdit] = useState(false);
+
+  return (
+    <div>
+      {!isEdit ? (
+        <div className='flex justify-between'>
+          <div>
+            <p>사용자 정보</p>
+            <p>아이디 : { state.user.userid }</p>
+            <p>사용자 이름 : {state.user.username}</p>
+            <p>이메일 : {state.user.email}</p>
+            <p>전화번호 : {state.user.telephone}</p>
+          </div>
+          <div>
+            <button className='border py-1 px-2 text-xs font-bold rounded-lg' onClick={() => {setisEdit(true)}}>편집</button>
+          </div>
+        </div>
+      ) : (
+        <EditUserProfile user={state.user} setisEdit={setisEdit} />
+      )}
+    </div>
+  );
+}
+
