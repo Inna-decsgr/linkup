d418d2c8857e588cbb9e6716c0f854f3507703ae
Inna-decsgr <anjgkwl27@gmail.com>
2025-03-24 17:54:48 +0900

    enhance: Add redirect to /login after signup and slightly improve input field styles

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Signup() {
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
     username: '',
     userid: '',
@@ -27,7 +29,9 @@ export default function Signup() {
 
       const data = await response.json();
       if (response.ok) {
+        console.log('회원가입한 사람', data);
         alert('회원가입 성공!');
+        navigate('/login')
       } else {
         alert(`회원가입 실패: ${data.message}`);
       }
@@ -39,51 +43,66 @@ export default function Signup() {
 
   return (
     <div>
-      <p>회원가입</p>
+      <p className='font-bold'>회원가입</p>
       <form onSubmit={handleSubmit}>
         <div>
-          <label>사용자 이름</label>
-          <input
-            type="text"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-            required
-          />
-          <label>아이디</label>
-          <input
-            type="text"
-            name="userid"
-            value={formData.userid}
-            onChange={handleChange}
-            required
-          />
-          <label>이메일</label>
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-          <label>비밀번호</label>
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-          <label>전화번호</label>
-          <input
-            type="tel"
-            name="telephone"
-            value={formData.telephone}
-            onChange={handleChange}
-            required
-          />
+          <div>
+            <label>사용자 이름</label>
+            <input
+              type="text"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+              className='border ml-2 rounded-sm'
+              required
+            />
+          </div>
+          <div>
+            <label>아이디</label>
+            <input
+              type="text"
+              name="userid"
+              value={formData.userid}
+              onChange={handleChange}
+              className='border ml-2 rounded-sm'
+              required
+            />
+          </div>
+          <div>
+            <label>이메일</label>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className='border ml-2 rounded-sm'
+              required
+            />
+          </div>
+          <div>
+            <label>비밀번호</label>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              className='border ml-2 rounded-sm'
+              required
+            />
+          </div>
+          <div>
+            <label>전화번호</label>
+            <input
+              type="tel"
+              name="telephone"
+              value={formData.telephone}
+              onChange={handleChange}
+              className='border ml-2 rounded-sm'
+              required
+            />
+          </div>
         </div>
-        <button type='submit'>회원가입</button>
+        <button type='submit' className='border w-[500px] py-2 px-3 text-sm font-bold mt-7'>회원가입</button>
       </form>
     </div>
   );
