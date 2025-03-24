6e26724d1eb66eddb7a473d48454769db42d875a
Inna-decsgr <anjgkwl27@gmail.com>
2025-03-24 17:56:52 +0900

    feat: Add profile image display feature to usersetting page

--- frontend/src/pages/UserSetting.jsx
+++ frontend/src/pages/UserSetting.jsx
@@ -6,6 +6,9 @@ import EditUserProfile from '../components/EditUserProfile';
 export default function UserSetting() {
   const { state } = useAuth();
   const [isEdit, setisEdit] = useState(false);
+  const profileImageUrl = state.user.profile_image === 'default_profile.png'
+  ? `/images/default_profile.png`
+  : `http://localhost:5000/images/${state.user.profile_image}`;
 
   return (
     <div>
@@ -13,6 +16,7 @@ export default function UserSetting() {
         <div className='flex justify-between'>
           <div>
             <p>사용자 정보</p>
+            <img src={profileImageUrl} alt="프로필 이미지" className='w-[100px] h-[100px] object-cover rounded-full'/>
             <p>아이디 : { state.user.userid }</p>
             <p>사용자 이름 : {state.user.username}</p>
             <p>이메일 : {state.user.email}</p>
