82ed058b7961c72f2c0eac7a6f8c3a002f02bd75
Inna-decsgr <anjgkwl27@gmail.com>
2025-03-24 17:58:00 +0900

    feat: Add user profile image and link to account page in Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { useAuth } from '../context/AuthContext';
+
 
 export default function Navbar() {
+  const { state } = useAuth();
+  const profileImageUrl = state.user.profile_image === 'default_profile.png'
+  ? `/images/default_profile.png`
+  : `http://localhost:5000/images/${state.user.profile_image}`;
+
   return (
     <nav className="w-full bg-red-100 p-4 mt-auto flex justify-center">
-      <div className='max-w-screen-lg w-full flex justify-around'>
-        <Link to="/" className="mr-4">home</Link>
-        <button>돋보기(검색)</button>
-        <button>새 게시글 작성</button>
-        <button>영상 아이콘</button>
-        <Link to="/profile">프로필</Link>
+      <div className='max-w-screen-xl w-full flex justify-around items-center'>
+        <Link to="/" className="mr-4"><i className="fa-solid fa-house"></i></Link>
+        <button><i className="fa-solid fa-magnifying-glass"></i></button>
+        <button><i className="fa-solid fa-square-plus"></i></button>
+        <button><i className="fa-solid fa-video"></i></button>
+        <Link to="/profile">
+          <img src={profileImageUrl} alt="프로필 이미지" className='w-[40px] h-[40px] object-cover rounded-full'/>
+        </Link>
         <p></p>
       </div>
     </nav>
