0c1fbd48a328cd01fdfd4f8282e1ac4ed6de8d6a
Inna-decsgr <anjgkwl27@gmail.com>
2025-03-26 18:53:42 +0900

    Set up main app structure with routes, layout and providers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,40 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
-import Navbar from  './components/Navbar'
 import Home from './pages/Home';
 import Topbar from './components/Topbar';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import { AuthProvider } from './context/AuthContext';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import UserSetting from './pages/UserSetting';
+import UserSearch from './pages/UserSearch';
+import NewPost from './pages/NewPost';
+import PostDetails from './pages/PostDetails';
+import ProfilePage from './pages/ProfilePage';
+
 
 
 function App() {
   return (
-    <div className='min-h-screen flex flex-col items-center'>
-      <Router>
-        <Topbar />
-        <div className='flex-1 w-full max-w-screen-xl p-6'>
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </div>
-        <Navbar />
-      </Router>
-    </div>
+    <AuthProvider>
+      <div className='min-h-screen flex flex-col items-center'>
+        <Router>
+          <Topbar />
+          <div className='flex-1 w-full max-w-screen-xl p-8'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/profile/:userid" element={<ProfilePage />} />
+              <Route path="/settings" element={<UserSetting />} />
+              <Route path="/search" element={<UserSearch />} />
+              <Route path="/newpost" element={<NewPost />} />
+              <Route path="/newpost/details" element={<PostDetails />} />
+            </Routes>
+          </div>
+        </Router>
+      </div>
+    </AuthProvider>
   );
 }
 
