e2c9b17449fc30fc8afe3dbb80e2c43c225d5c51
Inna-decsgr <anjgkwl27@gmail.com>
2025-03-29 22:16:24 +0900

    refactor: update UserSearch to pass selected user data to parent component

--- frontend/src/components/UserTag.jsx
+++ frontend/src/components/UserTag.jsx
@@ -1,14 +1,13 @@
-import React
-
-  from 'react';
+import React from 'react';
 import UserSearch from './UserSearch';
 
-export default function UserTag({ cancel }) {
+export default function UserTag({ cancel, onSelectedUser }) {
+
   return (
     <div>
       태그할 사용자 찾을 컴포넌트
       <div>
-        <UserSearch cancel={cancel} istag={true} />
+        <UserSearch cancel={cancel} istag={true} onSelectUser={onSelectedUser} />
       </div>
     </div>
   );
