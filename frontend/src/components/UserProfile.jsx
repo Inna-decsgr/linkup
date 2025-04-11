8e09c7d62ec4247d73020528cfa930bcd82e1b33
Inna-decsgr <anjgkwl27@gmail.com>
2025-04-11 17:26:56 +0900

    refactor: Refactor UserProfile to fetch and handle user-related data directly

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -11,9 +11,11 @@ export default function UserProfile({ user, isMe }) {
   const { state } = useAuth();
   // 이때 state는 로그인한 사용자, props로 전달받은 정보는 다른 사용자에 대한 정보, 팔로우 정보 표시할 때 사용
   const navigate = useNavigate();
-  const [showallposts, setAllPosts] = useState(true);
-  const [showuserinfo, setUserInfo] = useState(false);
-  const [isFollowing, setIsFollowing] = useState(false);
+  const [isFollowing, setIsFollowing] = useState(false);  // 팔로잉 여부
+  const [activeSection, setActiveSection] = useState('posts');  // 컴포넌트 섹션 상태
+  const [allposts, setAllposts] = useState([]);  // 사용자가 작성한 모든 게시물 보여줄 때 넘겨주는 정보를 allposts에 저장
+  const [multiple, setMultiple] = useState(false);  // 해당 게시물에 게시된 이미지가 한 장인지 여러장인지 구분하는 상태
+  const [bookmarkedpost, setBookmarkedPost] = useState([]);  // 사용자가 북마크한 게시물들 보여줄 때 넘겨주는 정보를 bookmarkedpost에 저장
   const profileImageUrl = user?.profile_image === 'default_profile.png'
   ? `/images/default_profile.png`
     : `http://localhost:5000/images/${user?.profile_image}`;
@@ -36,15 +38,41 @@ export default function UserProfile({ user, isMe }) {
       checkFollowStatus();
     }
   }, [user?.id, state.user.id]);
+
+
+  useEffect(() => {
+    const fetchUserPosts = async () => {
+      try {
+        const res = await fetch(`http://localhost:5000/api/posts/${state.user?.id}`);
+        const data = await res.json();
+        console.log('게시물222', data);
+        // 사용자가 작성한 게시물들 가져올 때 해당 게시물에 첨부된 이미지가 한장인지 여러장인지 판단
+        const hasMultipleImages = data.postResults.some(post => post.images?.length > 1);
+        setMultiple(hasMultipleImages);
+
+        // 사용자가 작성한 게시물들 가져와서 단순화한 다음 allpost에 저장
+        const simplified = data.postResults.map(post => ({
+          firstimage: post.images?.[0],
+          postid: post.id
+        }));
+        setAllposts(simplified);
+        console.log(`${state.user?.id}가 작성한 게시물들 조회하기`, simplified);
+
+        // 사용자가 북마크한 게시물들 가져와서 단순화한 다음 bookmarkedpost에 저장
+        const bookmarksimplified = data.bookmarkedPosts.map(post => ({
+          firstimage: post.images?.[0],
+          postid: post.id
+        }))
+        setBookmarkedPost(bookmarksimplified);
+        console.log('사용자가 북마크한 게시물 단순화 결과', bookmarksimplified);
+      } catch (err) {
+        console.error('게시물 조회 중 오류 발생', err);
+      }
+    };
+
+    fetchUserPosts();
+  }, [state.user?.id, state.user?.userid]);
   
-  const showAllUserPosts = () => {
-    setAllPosts(true);
-    setUserInfo(false);
-  };
-  const showUserInfo = () => {
-    setAllPosts(false);
-    setUserInfo(true);
-  };
 
   const toggleFollow = async () => {
     const response = await fetch('http://localhost:5000/api/follow', {
@@ -98,21 +126,30 @@ export default function UserProfile({ user, isMe }) {
         </div>
       </div>
       <div className='flex w-[500px] mx-auto my-4 p-2'>
-        <button className='basis-1/2' onClick={showAllUserPosts}>
-          <span className={`inline-block px-3 ${showallposts ? 'border-b-2 border-black' : 'border-b-2 border-transparent'}`}>
+        <button className='basis-1/2' onClick={() => setActiveSection('posts')}>
+          <span className={`inline-block px-3 ${activeSection === 'posts' ? 'border-b-2 border-black' : 'border-b-2 border-transparent'}`}>
             <i className="fa-solid fa-camera-retro text-xl"></i>
           </span>
         </button>
-        <button className='basis-1/2' onClick={showUserInfo}>
-          <span className={`inline-block px-3 ${showuserinfo ? 'border-b-2 border-black' : 'border-b-2 border-transparent'}`}>
+        <button className='basis-1/2' onClick={() => setActiveSection('bookmarks')}>
+          <span className={`inline-block px-3 ${activeSection === 'bookmarks' ? 'border-b-2 border-black' : 'border-b-2 border-transparent'}`}>
+            <i className="fa-regular fa-bookmark text-xl"></i>
+          </span>
+        </button>
+        <button className='basis-1/2' onClick={() => setActiveSection('userinfo')}>
+          <span className={`inline-block px-3 ${activeSection === 'userinfo' ? 'border-b-2 border-black' : 'border-b-2 border-transparent'}`}>
             <i className="fa-regular fa-user text-xl"></i>
           </span>
         </button>
       </div>
       <div className='w-[500px] mx-auto mt-5'>
-        {showallposts ? (
-          <UserAllPosts user_id={user?.id} userid={user?.userid} />
-        ): (
+        {activeSection === 'posts' && (
+          <UserAllPosts user_id={user?.id} userid={user?.userid} post={allposts} multiple={multiple}/>
+        )}
+        {activeSection === 'bookmarks' && (
+          <UserAllPosts user_id={user?.id} userid={user?.userid} post={bookmarkedpost} multiple={multiple}/>
+        )}
+        {activeSection === 'userinfo' && (
           <p>사용자 정보 컴포넌트</p>
         )}
       </div>
