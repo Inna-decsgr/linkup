c75aadda1e688fe0c9b3ba158347261ef34c50aa
Inna-decsgr <anjgkwl27@gmail.com>
2025-04-16 18:24:52 +0900

    feat: load search history on initial render and apply user card component

--- frontend/src/components/UserSearch.jsx
+++ frontend/src/components/UserSearch.jsx
@@ -2,7 +2,7 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from '../components/ui/Button';
 import { useAuth } from '../context/AuthContext';
-import { Imageformat } from '../utils/Imageformat';
+import SearchUserCard from './SearchUserCard';
 
 
 
@@ -10,6 +10,7 @@ export default function UserSearch({cancel, istag, onSelectUser}) {
   const navigate = useNavigate();
   const [keyword, setKeyword] = useState('');
   const [debouncedKeyword, setDebouncedKeyword] = useState('');
+  const [history, setHistory] = useState([]);
   const [results, setResults] = useState([]);
   const { state } = useAuth();
 
@@ -34,6 +35,29 @@ export default function UserSearch({cancel, istag, onSelectUser}) {
       console.error('서버 오류:', error);
     }
   }, [state.user?.userid]);
+
+
+  const handlenavigate = (user) => {
+    // 기존 검색 기록 가져와서 중복 제거 후 filtered에 저장
+    const existing = JSON.parse(localStorage.getItem('searchhistory')) || [];
+    const filtered = existing.filter(item => item.userid !== user.userid);
+    // 맨 앞에 새로 검색한 사용자 추가
+    const updated = [user, ...filtered];
+    // 새로 검색한 사용자가 포함된 updated 배열을 로컬 스토리지에 저장
+    localStorage.setItem('searchhistory', JSON.stringify(updated));
+    // 로컬스토리지에서 검색 기록 가져와서 stored에 저장 후 history 상태에 저장
+    const stored = JSON.parse(localStorage.getItem('searchhistory')) || [];
+    console.log('검색기록에 추가된 사용자', stored);
+    setHistory(stored);
+    navigate(`/profile/${user.userid}/${user.id}/${user.username}`)
+  }
+
+  useEffect(() => {
+    const stored = JSON.parse(localStorage.getItem('searchhistory')) || [];
+    setHistory(stored);
+    console.log('검색 기록', stored);
+  }, []);
+
 
 
   // 디바운스 처리
@@ -64,7 +88,7 @@ export default function UserSearch({cancel, istag, onSelectUser}) {
   return (
     <div>
       <div>
-        <div>
+        <div className='flex'>
           <input
             type="text"
             value={keyword}
@@ -72,31 +96,20 @@ export default function UserSearch({cancel, istag, onSelectUser}) {
             className='border w-[300px] outline-none'
             placeholder='검색'
           />
+          <Button text="취소" width="w-[60px]" onClick={cancel}/>
         </div>
-        <Button text="취소" width="w-[60px]" onClick={cancel}/>
       </div>
       <div>
         {setResults.length > 0 ? (
           results.map((user, index) => (
-            <div key={index} className='flex items-center justify-between'>
-              <div className='flex cursor-pointer' onClick={() => navigate(`/profile/${user.userid}/${user.id}/${user.username}`)}>
-                <div>
-                  <img src={Imageformat(user.profile_image)}
-                  alt="프로필 이미지"
-                  className="w-[50px] h-[50px] rounded-full object-cover"
-                  />
-                </div>
-                <div>
-                  <p>{user.userid}</p>
-                  <p>{user.username}</p>
-                </div>
+            <div key={index} className='flex items-center justify-between cursor-pointer' onClick={() => handlenavigate(user)}>
+              <div>
+                <SearchUserCard user={user} />
               </div>
               <div>
-                <div>
-                  {istag && (
-                    <Button text="선택" width="w-[60px]" onClick={() => selectUser(user)}/>
-                  )}
-                </div>
+                {istag && (
+                  <Button text="선택" width="w-[60px]" onClick={() => selectUser(user)}/>
+                )}
               </div>
             </div>
           ))
@@ -106,6 +119,21 @@ export default function UserSearch({cancel, istag, onSelectUser}) {
           </div>
         )}
       </div>
+      {!istag && history.length > 0 && (
+        <div className='mt-7'>
+          <div className='flex items-center justify-between text-sm'>
+            <p>최근 검색</p>
+            <button>모두 보기</button>
+          </div>
+          <div>
+            {history.map((user, index) => (
+              <div key={index}>
+                <SearchUserCard user={user} history={true} setHistory={setHistory} />
+              </div>
+            ))}
+        </div>
+      </div>
+      )}
     </div>
   );
 }
