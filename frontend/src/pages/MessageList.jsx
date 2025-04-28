19a7b871db08e82f9b0ab4bc7ad105556ec41dc5
Inna-decsgr <anjgkwl27@gmail.com>
2025-04-28 18:56:02 +0900

    feat: add navigation to dm page with partner info on click

--- frontend/src/pages/MessageList.jsx
+++ frontend/src/pages/MessageList.jsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { useAuth } from '../context/AuthContext';
 import { Imageformat } from '../utils/Imageformat';
 import { formatTimeAgo } from '../utils/formatTimeage';
+import { useNavigate } from 'react-router-dom';
 
 export default function MessageList() {
   const { state } = useAuth();
+  const navigate = useNavigate();
   const [messagelist, setMessageList] = useState([]);
 
   useEffect(() => {
@@ -25,7 +27,7 @@ export default function MessageList() {
       <div>
         {messagelist.map((msg, index) => {
           return (
-            <div key={index} className='flex items-center border p-2 rounded-md'>
+            <div key={index} className='flex items-center border p-2 rounded-md cursor-pointer' onClick={() => navigate(`/dm/${state.user?.id}/${msg.partner_id}`, {state: {partnername:msg.username, partner_id:msg.userid, profileimage: msg.profile_image}})}>
               <img src={Imageformat(msg.profile_image)} alt="상대방 프로필 이미지" className='w-[50px] h-[50px] rounded-full object-cover'/>
               <div className='pl-2'>
                 <p className='text-sm'>{msg.username}</p>
