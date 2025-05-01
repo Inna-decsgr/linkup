a63744df5f2db62bb629088accb622ef2a7d242d
Inna-decsgr <anjgkwl27@gmail.com>
2025-05-01 19:41:31 +0900

    fix: correct typo in formatTimeago function name and update references across components

--- frontend/src/pages/MessageList.jsx
+++ frontend/src/pages/MessageList.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useAuth } from '../context/AuthContext';
 import { Imageformat } from '../utils/Imageformat';
-import { formatTimeAgo } from '../utils/formatTimeage';
+import { formatTimeAgo } from '../utils/formatTimeago';
 import { useNavigate } from 'react-router-dom';
 
 export default function MessageList() {
